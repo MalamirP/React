80873cf70dc354ea406f6fdf891eb8e2545b428c
Malamir P <mimoe@abv.bg>
2023-03-05 16:08:56 +0200

    ad edit new task

---
+++ Test-app/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import React from 'react'
+
+function App() {
+  // const number = 1;
+  // React.useEffect(() => {
+  //   console.log("Effect function ran")
+  // }, [number])
+
+  const [user, setUser] = React.useState({
+    name: '',
+    lastName: '',
+    email: '',
+    city: ''
+  });
+  console.log(user);
+  const number = 2;
+
+
+  number < 0 ? (console.log('less than zero')) :
+    console.log('Greater than zero')
+
+  function handleChange(event) {
+    const { name, lastName, email, city } = event.target
+    setUser(user => {
+      return {
+        ...user,
+      }
+    })
+  }
+
+  return (
+    <div className="App">
+      <input type='text'
+        name='name'
+        onChange={handleChange}
+        placeholder='name'
+        value={user.name}
+      />
+      <input type='text'
+        name='lastName'
+        placeholder='lastName'
+        value={user.lastName}
+        onChange={handleChange}
+
+      />
+      <input type='text'
+        name='email'
+        placeholder='email'
+        value={user.email}
+        onChange={handleChange}
+      />
+      <input type='text'
+        name='city'
+        placeholder='city'
+        value={user.city}
+        onChange={handleChange}
+      />
+      <button
+      >Sumbit</button>
+    </div>
+  );
+}
+
+export default App;
